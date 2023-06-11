d9f26c7ffeb550bc2b2497796f2a65091e2cc17e
edgone <edgtavares92@gmail.com>
2023-06-11 10:33:19 +0100

    Added configs and testing migrations

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <Link href="/">Main Page</Link>
+  <Link href="/hello">Show Page</Link>
+  <div>The page with {{ timer }}s</div>
+  <slot />
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+import { ref } from 'vue'
+
+const timer = ref(0)
+
+setInterval(() => timer.value++, 1000)
+
+</script>
+
