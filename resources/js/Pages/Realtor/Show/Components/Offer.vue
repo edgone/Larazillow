81ab140cebecd74319abb40becb44394c05cbd0f
edgone <edgtavares92@gmail.com>
2023-09-07 00:16:24 +0100

    Finished Listing Offers

--- resources/js/Pages/Realtor/Show/Components/Offer.vue
+++ resources/js/Pages/Realtor/Show/Components/Offer.vue
@@ -1,18 +1,31 @@
 <template>
     <Box>
-        <template #header>Offer #{{ offer.id }}</template>
+        <template #header
+            >Offer #{{ offer.id }}
+            <span
+                v-if="offer.accepted_at"
+                class="dark:bg-green-900 dark:text-green-200 bg-green-200 text-green-900 px-2 py-1 rounded-md uppercase ml-1 font-xs"
+                >accepted</span
+            ></template
+        >
 
-        <section class="flex items-center justify-between">
+        <section class="flex items-start justify-between">
             <div>
                 <Price :price="offer.amount" class="text-xl" />
                 <div class="text-gray-500">Difference <Price :price="difference" /></div>
-
-                <div class="text-gray-500 text-sm">Made by John Doe</div>
-
+                <div class="text-gray-500 text-sm">Made by {{ offer.bidder.name }}</div>
                 <div class="text-gray-500 text-sm">Made on {{ madeOn }}</div>
             </div>
             <div>
-                <Link class="btn-outline text-xs font-medium" as="button">Accept</Link>
+                <Link
+                    v-if="!isSold"
+                    :href="route('realtor.offer.accept', { offer: offer.id })"
+                    class="btn-outline text-xs font-medium"
+                    as="button"
+                    method="put"
+                >
+                    Accept
+                </Link>
             </div>
         </section>
     </Box>
@@ -27,8 +40,10 @@ import { computed } from "vue";
 const props = defineProps({
     offer: Object,
     listingPrice: Number,
+    isSold: Boolean,
 });
 
 const difference = computed(() => props.offer.amount - props.listingPrice);
+
 const madeOn = computed(() => new Date(props.offer.created_at).toLocaleDateString());
 </script>
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
 use App\Http\Controllers\UserAccountController;
 use App\Http\Controllers\ListingOfferController;
+use App\Http\Controllers\RealtorListingAcceptOfferController;
 use App\Http\Controllers\RealtorListingController;
 use App\Http\Controllers\RealtorListingImageController;
 
@@ -47,6 +48,8 @@ Route::prefix('realtor')
     Route::resource('listing', RealtorListingController::class)
       // ->only(['index', 'create', 'store', 'edit', 'update', 'destroy'])
       ->withTrashed();
+
+    Route::name('offer.accept')->put('offer/{offer}/accept', RealtorListingAcceptOfferController::class);
 
     Route::resource('listing.image', RealtorListingImageController::class)
       ->only(['create', 'store', 'destroy']);
